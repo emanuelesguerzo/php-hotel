3ca4550aa0e7bea8f4c7564a665628444b616a1a
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2025-03-19 16:30:09 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>PHP Hotels</title>
+
+</head>
+<body>
+
+    <!-- Main Title -->
+    <h1>Elenco degli Hotel</h1>
+
+    <!-- Table -->
+    <table>
+
+        <!-- Table Head -->
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Descrizione</th>
+                <th>Parcheggio</th>
+                <th>Voto</th>
+                <th>Distanza dal centro</th>
+            </tr>
+        </thead>
+
+        <!-- Table Body -->
+        <tbody>
+            <?php foreach ($hotels as $hotel) : ?>
+
+               <tr>
+                    <td><?php echo $hotel["name"] ?></td>
+                    <td><?php echo $hotel["description"] ?></td>
+                    <td><?php echo $hotel["parking"] ?></td>
+                    <td><?php echo $hotel["vote"] ?></td>
+                    <td><?php echo $hotel["distance_to_center"] ?></td>
+               </tr> 
+
+            <?php endforeach; ?>
+        </tbody>
+
+    </table>
+    
+</body>
+</html>
